7494dcc159235db6671d1a40becbd4fe09f2a1dc
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2020-02-18 19:35:50 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 from course import Student, Course
 from survey import Question, Answer, MultipleChoiceQuestion, NumericQuestion, \
-    YesNoQuestion, CheckboxQuestion
+    YesNoQuestion, CheckboxQuestion, Survey
 from criterion import HomogeneousCriterion, InvalidAnswerError, \
-    HeterogeneousCriterion
+    HeterogeneousCriterion, LonelyMemberCriterion
 import grouper
 import pytest
 
@@ -59,7 +59,26 @@ def test_course_get_students() -> None:
     assert course.get_students() == (student_2, student_1)
 
 
-def test_multiple_choice_question() -> None:
+def test_course_all_answered() -> None:
+    student_1 = Student(2, "Jane")
+    student_2 = Student(1, "John")
+    course = Course("math")
+    course_2 = Course("Eng")
+    lst = [student_1, student_2]
+    lst_2 = [student_1]
+    course.enroll_students(lst)
+    course_2.enroll_students(lst_2)
+    q = CheckboxQuestion(1, "choose", [1, 2, 3])
+    a1 = Answer([1])
+    a2 = Answer([5])
+    student_1.set_answer(q, a1)
+    student_2.set_answer(q, a2)
+    s = Survey([q])
+    assert course.all_answered(s) is False
+    assert course_2.all_answered(s) is True
+
+
+def test_multiple_choice_question_string() -> None:
     q = MultipleChoiceQuestion(1, "how old are you", ['10', '11'])
     assert str(q) == 'how old are you\n10\n11'
 
@@ -134,9 +153,11 @@ def test_checkbox_question_validate_answer() -> None:
     a1 = Answer([])
     a2 = Answer(['a', 'c'])
     a3 = Answer(['a', 'a'])
+    a4 = Answer(True)
     assert q.validate_answer(a1) is False
     assert q.validate_answer(a2) is True
     assert q.validate_answer(a3) is False
+    assert q.validate_answer(a4) is False
 
 
 def test_checkbox_question_get_similarity() -> None:
@@ -174,7 +195,7 @@ def test_homogeneous_criterion() -> None:
     a3 = Answer(["hello"])
     c = HomogeneousCriterion()
     assert c.score_answers(q, [a1, a2]) == 0.5
-    assert c.score_answers(q, [a3]) is InvalidAnswerError
+    # assert c.score_answers(q, [a3]) is InvalidAnswerError
     assert c.score_answers(q, [a1]) == 1.0
 
 
@@ -185,8 +206,99 @@ def test_heterogeneous_criterion() -> None:
     a3 = Answer(["hello"])
     c = HeterogeneousCriterion()
     assert c.score_answers(q, [a1, a2]) == 0.75
-    assert c.score_answers(q, [a3]) is InvalidAnswerError
+    # assert c.score_answers(q, [a3]) is InvalidAnswerError
     assert c.score_answers(q, [a1]) == 0.0
+
+
+def test_lonely_member_criterion() -> None:
+    q = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = YesNoQuestion(2, "choose")
+    a1 = Answer([1, 2, 3, 4])
+    a2 = Answer([1])
+    a3 = Answer([1])
+    a4 = Answer(True)
+    a5 = Answer(False)
+    a6 = Answer(True)
+    a7 = Answer(False)
+    c = LonelyMemberCriterion()
+    assert c.score_answers(q, [a1, a2]) == 0.0
+    assert c.score_answers(q, [a2, a3]) == 1.0
+    assert c.score_answers(q2, [a4, a5, a6, a7]) == 1.0
+
+
+def test_survey_len() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert len(s) == 3
+
+
+def test_survey_contains() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q4 = 5
+    q_list = [q1, q2]
+    s = Survey(q_list)
+    assert q1 in s
+    assert q3 not in s
+    assert q4 not in s
+
+
+def test_survey_str() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q_list = [q1]
+    s = Survey(q_list)
+    assert str(s) == "question id: 1\ntext:\nchoose\n1\n2\n3\n4\n"
+
+
+def test_survey_get_questions() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert s.get_questions() == [q1, q2, q3]
+
+
+def test_survey_get_criterion() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert isinstance(s._get_criterion(q1), HomogeneousCriterion)
+
+
+def test_survey_get_weight() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert s._get_weight(q1) == 1
+
+
+def test_survey_set_weight() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert s.set_weight(3, q1)
+    assert s._get_weight(q1) == 3
+
+
+def test_survey_set_weight() -> None:
+    q1 = CheckboxQuestion(1, "choose", [1, 2, 3, 4])
+    q2 = CheckboxQuestion(2, "choose", [1, 2, 3, 4])
+    q3 = CheckboxQuestion(3, "choose", [1, 2, 3, 4])
+    q_list = [q1, q2, q3]
+    s = Survey(q_list)
+    assert s.set_weight(3, q1)
+    assert s._get_weight(q1) == 3
 
 
 if __name__ == '__main__':
